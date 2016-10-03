433eab4e8143640491fabef3efe9f4dea7162cce
Brian Wagner <briwagner@outlook.com>
2016-10-03 15:28:13 -0400

    add bare skeleton for featured

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './main-app/app.component';
 import { HeaderComponent } from './header/header.component';
 import { ColorboxComponent } from './colorbox/colorbox.component';
+import { FeaturedComponent } from './featured/featured.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ColorboxComponent
+    ColorboxComponent,
+    FeaturedComponent
   ],
   imports: [
     BrowserModule,
